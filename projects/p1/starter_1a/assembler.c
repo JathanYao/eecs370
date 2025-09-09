e5c70c8566ee002b88b73ba63506e1d2ddcd889b
JathanYao <jathansjyao@gmail.com>
2025-09-09 00:07:52 -0400

    add more functios

--- projects/p1/starter_1a/assembler.c
+++ projects/p1/starter_1a/assembler.c
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <string.h>
 
-//Every LC2K file will contain less than 1000 lines of assembly.
+// Every LC2K file will contain less than 1000 lines of assembly.
 #define MAXLINELENGTH 1000
 
 int readAndParse(FILE *, char *, char *, char *, char *, char *);
@@ -17,22 +17,23 @@ static void checkForBlankLinesInCode(FILE *inFilePtr);
 static inline int isNumber(char *);
 static inline void printHexToFile(FILE *, int);
 
-typedef struct 
+typedef struct
 {
     char label[MAXLINELENGTH];
     int line;
-}label_line;
+} label_line;
 
 int main(int argc, char **argv)
 {
     char *inFileString, *outFileString;
     FILE *inFilePtr, *outFilePtr;
     char label[MAXLINELENGTH], opcode[MAXLINELENGTH], arg0[MAXLINELENGTH],
-            arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
+        arg1[MAXLINELENGTH], arg2[MAXLINELENGTH];
 
-    if (argc != 3) {
+    if (argc != 3)
+    {
         printf("error: usage: %s <assembly-code-file> <machine-code-file>\n",
-            argv[0]);
+               argv[0]);
         exit(1);
     }
 
@@ -40,7 +41,8 @@ int main(int argc, char **argv)
     outFileString = argv[2];
 
     inFilePtr = fopen(inFileString, "r");
-    if (inFilePtr == NULL) {
+    if (inFilePtr == NULL)
+    {
         printf("error in opening %s\n", inFileString);
         exit(1);
     }
@@ -49,17 +51,18 @@ int main(int argc, char **argv)
     checkForBlankLinesInCode(inFilePtr);
 
     outFilePtr = fopen(outFileString, "w");
-    if (outFilePtr == NULL) {
+    if (outFilePtr == NULL)
+    {
         printf("error in opening %s\n", outFileString);
         exit(1);
     }
 
-    //FIRST PASS, IDENTIFY ALL LABELS
+    // FIRST PASS, IDENTIFY ALL LABELS
     label_line lab[MAXLINELENGTH];
     int pc = 0, count = 0;
-    while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2))
+    while (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2))
     {
-        if(strcmp(label, ""))
+        if (strcmp(label, ""))
         {
             lab[count].line = pc;
             strcpy(lab[count].label, label); // store line of label
@@ -72,67 +75,75 @@ int main(int argc, char **argv)
         beginning of the file */
     rewind(inFilePtr);
 
-    //SECOND PASS, GENERATE MACHINE LANGUAGE INSTRUCTIONS
+    // SECOND PASS, GENERATE MACHINE LANGUAGE INSTRUCTIONS
     int line = 0;
-    while(readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2))
+    while (readAndParse(inFilePtr, label, opcode, arg0, arg1, arg2))
     {
         int a0 = atoi(arg0), a1 = atoi(arg1), a2 = -1;
-        if(!isNumber(arg2)) //arg2 is a label, find line for the label from label_line
+        if (!isNumber(arg2)) // arg2 is a label, find line for the label from label_line
         {
-            for(int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
             {
-                if(!strcmp(lab[i].label, arg2))
+                if (!strcmp(lab[i].label, arg2))
                 {
-                  //found corresponding label
-                  a2 = lab[i].line;  
+                    // found corresponding label
+                    a2 = lab[i].line;
                 }
             }
-            if(a2 < 0)
+            if (a2 < 0)
             {
                 printf("Label undefined");
                 exit(1);
             }
-        }else a2 = atoi(arg2);
-        if(!strcmp(opcode, "add"))
+        }
+        else
+            a2 = atoi(arg2);
+
+        // ALL 8 FUNCTIONS
+        if (!strcmp(opcode, "add") || strcmp(opcode, "nor"))
         {
-            line += 0; //opcode for add is 000
+            if (!strcmp(opcode, "nor"))
+                line++;
             line = a0;
             line = (line << 3) + a1;
             line <<= 16;
             line += a2;
             printHexToFile(outFilePtr, 123);
-        }else if(!strcmp(opcode, "nor"))
+        }
+        else if (!strcmp(opcode, "lw") || !strcmp(opcode, "sw") || !strcmp(opcode, "beq"))
         {
-            line += 1; //opcode for 
-        }else if(!strcmp(opcode, "lw"))
+            line += 2; // case for lw
+            if (!strcmp(opcode, "sw"))
+                line++;
+            else if (!strcmp(opcode, "beq"))
+                line += 2;
+            line = (line << 3) + a0;
+            line = (line << 3) + a1;
+            line = (line << 15) + a2;
+        }
+        else if (!strcmp(opcode, "jalr"))
         {
-            
-        }else if(!strcmp(opcode, "sw"))
-        {   
-            
-        }else if(!strcmp(opcode, "beq"))
+            line += 5;
+        }
+        else if (!strcmp(opcode, "halt"))
         {
-            
-        }else if(!strcmp(opcode, "jalr"))
-        {
-            
-        }else if(!strcmp(opcode, "halt"))
-        {
-
-        }else //noop
+        }
+        else // noop
         {
         }
     }
- 
+
     /* after doing a readAndParse, you may want to do the following to test the
         opcode */
-    if (!strcmp(opcode, "add")) {
+    if (!strcmp(opcode, "add"))
+    {
         /* do whatever you need to do for opcode "add" */
     }
 
     /* here is an example of using isNumber. "5" is a number, so this will
        return true */
-    if(isNumber("5")) {
+    if (isNumber("5"))
+    {
         printf("It's a number\n");
     }
 
@@ -140,22 +151,27 @@ int main(int argc, char **argv)
        machine code word / number in the proper hex format to the output file */
     printHexToFile(outFilePtr, 123);
 
-    return(0);
+    return (0);
 }
 
 // Returns non-zero if the line contains only whitespace.
-static int lineIsBlank(char *line) {
+static int lineIsBlank(char *line)
+{
     char whitespace[4] = {'\t', '\n', '\r', ' '};
     int nonempty_line = 0;
-    for(int line_idx=0; line_idx < strlen(line); ++line_idx) {
+    for (int line_idx = 0; line_idx < strlen(line); ++line_idx)
+    {
         int line_char_is_whitespace = 0;
-        for(int whitespace_idx = 0; whitespace_idx < 4; ++ whitespace_idx) {
-            if(line[line_idx] == whitespace[whitespace_idx]) {
+        for (int whitespace_idx = 0; whitespace_idx < 4; ++whitespace_idx)
+        {
+            if (line[line_idx] == whitespace[whitespace_idx])
+            {
                 line_char_is_whitespace = 1;
                 break;
             }
         }
-        if(!line_char_is_whitespace) {
+        if (!line_char_is_whitespace)
+        {
             nonempty_line = 1;
             break;
         }
@@ -165,27 +181,35 @@ static int lineIsBlank(char *line) {
 
 // Exits 2 if file contains an empty line anywhere other than at the end of the file.
 // Note calling this function rewinds inFilePtr.
-static void checkForBlankLinesInCode(FILE *inFilePtr) {
+static void checkForBlankLinesInCode(FILE *inFilePtr)
+{
     char line[MAXLINELENGTH];
     int blank_line_encountered = 0;
     int address_of_blank_line = 0;
     rewind(inFilePtr);
 
-    for(int address = 0; fgets(line, MAXLINELENGTH, inFilePtr) != NULL; ++address) {
+    for (int address = 0; fgets(line, MAXLINELENGTH, inFilePtr) != NULL; ++address)
+    {
         // Check for line too long
-        if (strlen(line) >= MAXLINELENGTH-1) {
+        if (strlen(line) >= MAXLINELENGTH - 1)
+        {
             printf("error: line too long\n");
             exit(1);
         }
 
         // Check for blank line.
-        if(lineIsBlank(line)) {
-            if(!blank_line_encountered) {
+        if (lineIsBlank(line))
+        {
+            if (!blank_line_encountered)
+            {
                 blank_line_encountered = 1;
                 address_of_blank_line = address;
             }
-        } else {
-            if(blank_line_encountered) {
+        }
+        else
+        {
+            if (blank_line_encountered)
+            {
                 printf("Invalid Assembly: Empty line at address %d\n", address_of_blank_line);
                 exit(2);
             }
@@ -194,10 +218,9 @@ static void checkForBlankLinesInCode(FILE *inFilePtr) {
     rewind(inFilePtr);
 }
 
-
 /*
-* NOTE: The code defined below is not to be modifed as it is implemented correctly.
-*/
+ * NOTE: The code defined below is not to be modifed as it is implemented correctly.
+ */
 
 /*
  * Read and parse a line of the assembly-language file.  Fields are returned
@@ -210,9 +233,8 @@ static void checkForBlankLinesInCode(FILE *inFilePtr) {
  *
  * exit(1) if line is too long.
  */
-int
-readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
-    char *arg1, char *arg2)
+int readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
+                 char *arg1, char *arg2)
 {
     char line[MAXLINELENGTH];
     char *ptr = line;
@@ -221,26 +243,30 @@ readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
     label[0] = opcode[0] = arg0[0] = arg1[0] = arg2[0] = '\0';
 
     /* read the line from the assembly-language file */
-    if (fgets(line, MAXLINELENGTH, inFilePtr) == NULL) {
-	/* reached end of file */
-        return(0);
+    if (fgets(line, MAXLINELENGTH, inFilePtr) == NULL)
+    {
+        /* reached end of file */
+        return (0);
     }
 
     /* check for line too long */
-    if (strlen(line) == MAXLINELENGTH-1) {
-	printf("error: line too long\n");
-	exit(1);
+    if (strlen(line) == MAXLINELENGTH - 1)
+    {
+        printf("error: line too long\n");
+        exit(1);
     }
 
     // Ignore blank lines at the end of the file.
-    if(lineIsBlank(line)) {
+    if (lineIsBlank(line))
+    {
         return 0;
     }
 
     /* is there a label? */
     ptr = line;
-    if (sscanf(ptr, "%[^\t\n ]", label)) {
-	/* successfully read label; advance pointer over the label */
+    if (sscanf(ptr, "%[^\t\n ]", label))
+    {
+        /* successfully read label; advance pointer over the label */
         ptr += strlen(label);
     }
 
@@ -249,9 +275,9 @@ readAndParse(FILE *inFilePtr, char *label, char *opcode, char *arg0,
      * expressions, but scanf will suffice.
      */
     sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]",
-        opcode, arg0, arg1, arg2);
+           opcode, arg0, arg1, arg2);
 
-    return(1);
+    return (1);
 }
 
 static inline int
@@ -259,12 +285,12 @@ isNumber(char *string)
 {
     int num;
     char c;
-    return((sscanf(string, "%d%c",&num, &c)) == 1);
+    return ((sscanf(string, "%d%c", &num, &c)) == 1);
 }
 
-
 // Prints a machine code word in the proper hex format to the file
-static inline void 
-printHexToFile(FILE *outFilePtr, int word) {
+static inline void
+printHexToFile(FILE *outFilePtr, int word)
+{
     fprintf(outFilePtr, "0x%08X\n", word);
 }
