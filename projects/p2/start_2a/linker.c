8eddc201d50be82af22e08c0ec58965b31e5dcdd
Jathan Yao <jathansjyao@gmail.comcd>
2025-10-21 03:31:34 -0400

    done text section do data section later

--- projects/p2/start_2a/linker.c
+++ projects/p2/start_2a/linker.c
@@ -13,33 +13,12 @@
 // Every LC2K file will contain less than 1000 lines of assembly.
 #define MAXLINELENGTH 1000
 #define MAXADDRESSES 65535
-
-int readAndParse(FILE *, char *, char *, char *, char *, char *);
-static void checkForBlankLinesInCode(FILE *inFilePtr);
-static inline int isNumber(char *);
-static inline void printHexToFile(FILE *, int);
-
-typedef struct
-{
-    char label[MAXLINELENGTH];
-    int line;
-} label_line;
+int readHeader(FILE *inFilePtr, int *text, int* data, int* symbols, int* relocations);
+int readRelocation(FILE *inFilePtr, int* address, char* opcode, char* label);
+int readSymbol(FILE *inFilePtr, char* label, char* type, int* address);
+int readTextorData(FILE *inFilePtr, int *instr);
 
 
-typedef struct 
-{
-    char label[7];
-    char type;
-    int address;
-}symbol;
-
-typedef struct 
-{
-    int fromFile;
-    int address;
-    char opcode[7];
-    char label[7];
-}relocation;
 
 typedef struct 
 {
@@ -51,8 +30,19 @@ typedef struct
     int relocSize;
     int text[MAXLINELENGTH];
     int data[MAXLINELENGTH];
-    symbol symbolTable[MAXLINELENGTH];
-    relocation relocTable[MAXLINELENGTH];
+    struct 
+    {
+        char label[7];
+        char type;
+        int address;
+    }symbolTable[MAXLINELENGTH];
+    struct 
+    {
+        int fromFile;
+        int address;
+        char opcode[7];
+        char label[7];
+    }relocTable[MAXLINELENGTH];
 }File;
 
 
@@ -66,7 +56,7 @@ int main(int argc, char **argv)
         printf("error: usage: %s <file_0.obj> <file_1.obj> ... <file_N.obj> <machine_code.mc>\n", argv[0]);
         exit(1);
     }
-
+    int numFiles = argc - 2;
     outFileString = argv[argc - 1]; //mc file location
 
     outFilePtr = fopen(outFileString, "w");
@@ -77,9 +67,9 @@ int main(int argc, char **argv)
     }
 
     File files[6]; //max 6 files 
-    for(int i = 1; i < argc - 1; i++)//start at argv[1]
+    for(int i = 0; i < numFiles; i++)//start at argv[1]
     {
-        inFileString = argv[i]; // read in file
+        inFileString = argv[i + 1]; // read in file
         inFilePtr = fopen(inFileString, "r");
         if (inFilePtr == NULL)
         {
@@ -87,31 +77,30 @@ int main(int argc, char **argv)
             exit(1);
         }
 
-        char lines[MAXLINELENGTH];
         //read ins
-        {
+        
         //----------------------------------READ IN HEADER----------------------------------//
-        readHeader(inFilePtr, files[i].textSize, files[i].dataSize, files[i].symbolSize, files[i].relocSize);
+        readHeader(inFilePtr, &files[i].textSize, &files[i].dataSize, &files[i].symbolSize, &files[i].relocSize);
         //----------------------------------READ IN HEADER----------------------------------//
 
         //----------------------------------READ IN TEXT----------------------------------//
         for(int j = 0; j < files[i].textSize; j++)
         {
-            readTextorData(inFilePtr, files[i].text[j]);
+            readTextorData(inFilePtr, &files[i].text[j]);
         }
         //----------------------------------READ IN TEXT----------------------------------//
     
         //----------------------------------READ IN DATA----------------------------------//
         for(int j = 0; j < files[i].dataSize; j++)
         {
-            readTextorData(inFilePtr, files[i].data[j]);
+            readTextorData(inFilePtr, &files[i].data[j]);
         }
         //----------------------------------READ IN DATA----------------------------------//
 
         //----------------------------------READ IN SYMBOLS----------------------------------//
         for(int j = 0; j < files[i].symbolSize; j++)
         {
-            readSymbol(inFilePtr, files[i].symbolTable[j].label, files[i].symbolTable[j].type, files[i].symbolTable[j].address);
+            readSymbol(inFilePtr, files[i].symbolTable[j].label, &files[i].symbolTable[j].type, &files[i].symbolTable[j].address);
         }
         //----------------------------------READ IN SYMBOLS----------------------------------//
 
@@ -119,37 +108,36 @@ int main(int argc, char **argv)
         for(int j = 0; j < files[i].relocSize; j++)
         {
             files[i].relocTable[j].fromFile = i;
-            readRelocation(inFilePtr, files[i].relocTable[j].address, files[i].relocTable[j].opcode, files[i].relocTable[j].label);
-        }
+            readRelocation(inFilePtr, &files[i].relocTable[j].address, files[i].relocTable[j].opcode, files[i].relocTable[j].label);
+        
         //----------------------------------READ IN RELOCATIONS----------------------------------//
-        fclose(inFilePtr);
-        }
+    }
+    fclose(inFilePtr);
         //start changing
-
         int startLine = 0;
         //find starting points of text and data sections for each file
-        for(int i = 0; i < sizeof(files); i++)
+        for(int i = 0; i < numFiles; i++)
         {
             files[i].textStart = startLine;
             startLine += files[i].textSize;
         }
-        for(int i = 0; i < sizeof(files); i++)
+        for(int i = 0; i < numFiles; i++)
         {
             files[i].dataStart = startLine;
             startLine += files[i].dataSize;
         }
 
         //check global labels defined in > 1 files
-        for(int i = 0; i < sizeof(files) - 1; i++)
+        for(int i = 0; i < numFiles - 1; i++)
         {
             for(int j = 0; j < files[i].symbolSize; j++)
             {
-                for(int k = i + 1; k < sizeof(files); k++)
+                for(int k = i + 1; k < numFiles; k++)
                 {
                     for(int l = 0; l < files[k].symbolSize; l++)
                     {
                         bool sameLabel = (!strcmp(files[i].symbolTable[k].label, files[k].symbolTable[l].label));
-                        bool bothDefined = files[i].symbolTable[k].label != "U" && files[k].symbolTable[l].label != "U";
+                        bool bothDefined = files[i].symbolTable[k].type != 'U' && files[k].symbolTable[l].type != 'U';
                         if(sameLabel && bothDefined)
                         {
                             printf("Error: Duplicate Label");
@@ -159,9 +147,52 @@ int main(int argc, char **argv)
                 }
             }
         }
+        int ogOffset = 0, newOffset = 0;
+        //update text and data with relocations
+        for(int i = 0; i < numFiles; i++)
+        {
+            for(int j = 0; j < files[i].relocSize; j++)
+            {
+                //check text or data relocation
+                if(!strcmp(files[i].relocTable[j].opcode, "lw") || !strcmp(files[i].relocTable[j].opcode, "sw"))
+                {
+                    //in text section (lw, sw)
+                    char* label = files[i].relocTable[j].label;
+                    bool isLocal = islower(label[0]);
 
-        //fix all local labels
-        
+                    if(isLocal)
+                    {
+                        int instruction = files[i].text[files[i].relocTable[j].address];
+
+                        //find offset from instruction
+                        ogOffset = instruction & 0xF; //gets last hex bit
+
+                        // compute newoffset by adding offset to file start location in text block
+                        newOffset = ogOffset + files[i].textStart; 
+                    }
+                    else
+                    {
+                        //global label
+                        bool isStack = (!strcmp(label, "Stack"));
+                        if(isStack) //check if its the Stack label
+                        {
+                            //use startLine since it accumulated all text + data sizes
+                            newOffset = ogOffset + startLine; 
+                        }
+                        else
+                        {
+                            //find the global label value
+                            newOffset = getGlobal(files, numFiles, label);
+                        }
+                    }
+                }
+                else //in data section (.fill)
+                {
+
+                }
+                files[i].text[files[i].relocTable[j].address] = newOffset; //set the new computed offset
+            }
+        }
     }
 
     fclose(outFilePtr);
@@ -169,68 +200,32 @@ int main(int argc, char **argv)
     return (0);
 }
 
-// Returns non-zero if the line contains only whitespace.
-static int lineIsBlank(char *line)
+//returns value of the global label
+int getGlobal(File *files[6], int numFiles, char* label)
 {
-    char whitespace[4] = {'\t', '\n', '\r', ' '};
-    int nonempty_line = 0;
-    for (int line_idx = 0; line_idx < strlen(line); ++line_idx)
+    int value = -1; //use to error check
+    for(int i = 0; i < numFiles; i++)
     {
-        int line_char_is_whitespace = 0;
-        for (int whitespace_idx = 0; whitespace_idx < 4; ++whitespace_idx)
+        for(int j = 0; j < files[i]->symbolSize; j++)
         {
-            if (line[line_idx] == whitespace[whitespace_idx])
+            char type = files[i]->symbolTable->type;
+            switch(type)
             {
-                line_char_is_whitespace = 1;
-                break;
-            }
-        }
-        if (!line_char_is_whitespace)
-        {
-            nonempty_line = 1;
-            break;
-        }
-    }
-    return !nonempty_line;
-}
-
-// Exits 2 if file contains an empty line anywhere other than at the end of the file.
-// Note calling this function rewinds inFilePtr.
-static void checkForBlankLinesInCode(FILE *inFilePtr)
-{
-    char line[MAXLINELENGTH];
-    int blank_line_encountered = 0;
-    int address_of_blank_line = 0;
-    rewind(inFilePtr);
-
-    for (int address = 0; fgets(line, MAXLINELENGTH, inFilePtr) != NULL; ++address)
-    {
-        // Check for line too long
-        if (strlen(line) >= MAXLINELENGTH - 1)
-        {
-            printf("error: line too long\n");
-            exit(1);
-        }
-
-        // Check for blank line.
-        if (lineIsBlank(line))
-        {
-            if (!blank_line_encountered)
-            {
-                blank_line_encountered = 1;
-                address_of_blank_line = address;
-            }
-        }
-        else
-        {
-            if (blank_line_encountered)
-            {
-                printf("Invalid Assembly: Empty line at address %d\n", address_of_blank_line);
-                exit(2);
+                case 'T':
+                    value = files[i]->symbolTable->address + files[i]->textStart;
+                    break;
+                case 'D':
+                    value = files[i]->symbolTable->address + files[i]->dataStart;
+                    break;
             }
         }
     }
-    rewind(inFilePtr);
+    if(value == -1)
+    {
+        printf("Error: label never defined");
+        exit(1);
+    }
+    return value;
 }
 
 /*
@@ -245,8 +240,6 @@ static void checkForBlankLinesInCode(FILE *inFilePtr)
 int readHeader(FILE *inFilePtr, int *text, int* data, int* symbols, int* relocations)
 {
     char line[MAXLINELENGTH];
-    char *ptr = line;
-
     /* delete prior values */
     text[0] = data[0] = symbols[0] = relocations[0] = 0;
 
@@ -274,7 +267,7 @@ int readHeader(FILE *inFilePtr, int *text, int* data, int* symbols, int* relocat
      * Parse the rest of the line.  Would be nice to have real regular
      * expressions, but scanf will suffice.
      */
-    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]", text, data, symbols, relocations);
+    sscanf(line, "%d %d %d %d", text, data, symbols, relocations);
     return (1);
 }
 
@@ -290,7 +283,6 @@ int readHeader(FILE *inFilePtr, int *text, int* data, int* symbols, int* relocat
 int readTextorData(FILE *inFilePtr, int *instr)
 {
     char line[MAXLINELENGTH];
-    char *ptr = line;
     /* delete prior values */
     instr[0] = 0;
 
@@ -318,7 +310,7 @@ int readTextorData(FILE *inFilePtr, int *instr)
      * Parse the rest of the line.  Would be nice to have real regular
      * expressions, but scanf will suffice.
      */
-    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]", instr);
+    sscanf(line, "%x", instr);
     return (1);
 }
 
@@ -334,9 +326,8 @@ int readTextorData(FILE *inFilePtr, int *instr)
 int readSymbol(FILE *inFilePtr, char* label, char* type, int* address)
 {
     char line[MAXLINELENGTH];
-    char *ptr = line;
     /* delete prior values */
-    label[0] = type[0] = '/0';
+    label[0] = type[0] = '\0';
     address[0] = 0;
 
     /* read the line from the assembly-language file */
@@ -363,7 +354,7 @@ int readSymbol(FILE *inFilePtr, char* label, char* type, int* address)
      * Parse the rest of the line.  Would be nice to have real regular
      * expressions, but scanf will suffice.
      */
-    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]", label, type, address);
+    sscanf(line, "%6s %6s %d", label, type, address);
     return (1);
 }
 
@@ -380,9 +371,8 @@ int readSymbol(FILE *inFilePtr, char* label, char* type, int* address)
 int readRelocation(FILE *inFilePtr, int* address, char* opcode, char* label)
 {
     char line[MAXLINELENGTH];
-    char *ptr = line;
     /* delete prior values */
-    opcode[0] = label[0] = '/0';
+    opcode[0] = label[0] = '\0';
     address[0] = 0;
 
     /* read the line from the assembly-language file */
@@ -409,21 +399,6 @@ int readRelocation(FILE *inFilePtr, int* address, char* opcode, char* label)
      * Parse the rest of the line.  Would be nice to have real regular
      * expressions, but scanf will suffice.
      */
-    sscanf(ptr, "%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]%*[\t\n\r ]%[^\t\n\r ]", address, opcode, label);
+    sscanf(line, "%d %6s %6s", address, opcode, label);
     return (1);
-}
-
-static inline int
-isNumber(char *string)
-{
-    int num;
-    char c;
-    return ((sscanf(string, "%d%c", &num, &c)) == 1);
-}
-
-// Prints a machine code word in the proper hex format to the file
-static inline void
-printHexToFile(FILE *outFilePtr, int word)
-{
-    fprintf(outFilePtr, "0x%08X\n", word);
 }
